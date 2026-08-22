621fbe10040000d16a54e7d5144963f8801b3aa8
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-08-23 06:46:38 +0900

    Wire gallery preferences media actions and safety classifier

--- app/src/main/java/com/yanagikh/keepg/KeepGApplication.kt
+++ app/src/main/java/com/yanagikh/keepg/KeepGApplication.kt
@@ -2,10 +2,13 @@ package com.yanagikh.keepg
 
 import android.app.Application
 import com.yanagikh.keepg.advanced.AdvancedToolsFactory
+import com.yanagikh.keepg.data.GalleryPreferences
 import com.yanagikh.keepg.data.KeepGDatabase
+import com.yanagikh.keepg.data.MediaActionRepository
 import com.yanagikh.keepg.data.MediaStoreRepository
 import com.yanagikh.keepg.data.VaultRepository
 import com.yanagikh.keepg.debug.KeepGLog
+import com.yanagikh.keepg.media.SensitiveContentClassifier
 import com.yanagikh.keepg.security.VaultCipher
 import com.yanagikh.keepg.smart.FaceAnalysisEngine
 
@@ -17,7 +20,10 @@ class AppContainer(application: Application) {
     val log = KeepGLog(application)
     val database = KeepGDatabase.create(application)
     val dao = database.dao()
+    val preferences = GalleryPreferences(application)
     val mediaStore = MediaStoreRepository(application, dao, log)
+    val mediaActions = MediaActionRepository(application)
+    val sensitiveContent = SensitiveContentClassifier(application)
     val vault = VaultRepository(application, dao, VaultCipher())
     val faceAnalysis = FaceAnalysisEngine(application, dao)
     val advanced = AdvancedToolsFactory.create(application)
